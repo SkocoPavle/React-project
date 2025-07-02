a9427e0d6b80a24e8a435298b096648ebdd42194
Skoco Pavle <skocopavle@gmail.com>
2025-07-02 18:48:54 +0200

    Added JSX to the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,9 @@
-import "./styles.css"
-export default function APP () {
-  return "Hi"
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
