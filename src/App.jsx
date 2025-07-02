57d4b2b700ec32896078f86e9a81bd6fa8b4a6d3
Skoco Pavle <skocopavle@gmail.com>
2025-07-02 20:41:11 +0200

    Started adding JSX code

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,33 @@ import "./styles.css"
 
 export default function App () {
   const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
 
+  function handleSubmit (e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos, {id: crypto.randomUUID(), title: newItem, completed: false},
+      ]
+    })
+
+    setNewItem("")
+  } 
+
+  function toggleTodo (id, completed) {
+    setTodos (currentTodos => {
+      currentTodos.map(todo => {
+        if (todo.id === id) {
+          return {...todo, completed}
+        }
+
+        return todo
+      }) 
+    })
+  }
   return <>
-    <form className="new-item-form">
+    <form className="new-item-form" onSubmit={handleSubmit}>
       <div className="form-row">
         <label htmlFor="item">New item</label>
         <input id="item" type="text" value={newItem} onChange={e => setNewItem(e.target.value)} />
@@ -14,15 +38,20 @@ export default function App () {
     </form>
 
     <h1 className="header">Todo List</h1>
-
     <ul className="list">
-      <li>
-        <label>
-          <input type="checkbox" />
-          Item 1
-        </label>
-        <button className="btn btn-danger">Delete</button>
-      </li>
+      {todos.map(todo => (
+        <li key={todo.id}>
+          <label>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={e => toggleTodo(todo.id, e.target.checked)}
+            />
+            {todo.title}
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      ))}
     </ul>
   </>
 }
